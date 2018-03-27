93a7521f660b9b481da584ab0d9096ab60465aca
Kamalesh514 <37850980+Kamalesh514@users.noreply.github.com>
2018-03-27 17:25:51 -0500

    Add files via upload

---
+++ MazeSolver/Controllers/MazeController.cs
@@ -0,0 +1,40 @@
+﻿using MazeSolver.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MazeSolver.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Maze")]
+    public class MazeController : Controller
+    {
+        [HttpPost]
+        public string Solver([FromBody]MazeInput input)
+        {
+            return MazeSolver(input.Input);
+        }
+
+        private string MazeSolver(string input)
+        {
+            ////List<string> mapLines = input.Split(' ').ToList();
+            ////List<Cell> cells = new List<Cell>();
+            ////int x = 0;
+            ////int y = 0;
+            ////foreach(var line in mapLines)
+            ////{
+            ////    foreach (char c in line.ToCharArray())
+            ////    {
+            ////        var cell = new Cell
+            ////        {
+            ////            Top = x == 0 ? null : Getpoint
+
+            ////        };
+            ////        cells.Add(cell);
+            ////        y++;
+            ////    }
+            ////    x++;
+            ////}
+
+            return input + " - Maze is solved";
+        }
+    }
+}
---
+++ MazeSolver/Models/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MazeSolver.Models
+{
+    public class Cell
+    {
+        public Point? Top { get; set; }
+
+        public Point? Bottom { get; set; }
+
+        public Point? Left { get; set; }
+
+        public Point? Right { get; set; }
+
+        public Point? Current { get; set; }
+
+        public int XCoordinate { get; set; }
+
+        public int YCoordinate { get; set; }
+    }
+}
